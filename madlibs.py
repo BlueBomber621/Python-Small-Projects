eb06112d31b343d88c6ef36dc7ec449de2fba763
Blaine Curtis <bluebomber621@gmail.com>
2025-02-05 10:41:12 -0800

    First Projects

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+inputs = []
+inputs.append(input("Enter a noun: "))
+inputs.append(input("Enter a verb ending with ing: "))
+inputs.append(input("Enter an adjective: "))
+inputs.append(input("Enter another adjective: "))
+inputs.append(input("Enter a plural food noun: "))
+inputs.append(input("Enter a place name: "))
+
+print(f"Last weekend, I went camping with my family at {inputs[5]}. We saw a {inputs[0]} {inputs[1]}. \
+We had some {inputs[3]} {inputs[4]}. It was a {inputs[2]} trip!")
---
+++ nameage.py
@@ -0,0 +1,7 @@
+name = "Fred"
+age = 68
+
+print("Name: " + name)
+print(age)
+# print with adding, even if number
+print("Age: " + str(age))
